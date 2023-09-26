4b7c52309a3cfaa95c08fae2992fdd3bd62cdbf5
only-zover <j0seextremehd@outlook.com>
2023-09-26 20:45:04 -0300

    changing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+    import '@picocss/pico/css/pico.min.css'
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+    <h1>Teste</h1>
+</div>
